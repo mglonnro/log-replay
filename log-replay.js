8eff7e9bfa8151bb19c3e5abf4d5d42b2ef62a84
Mikael Lönnroth <mikael@humeko.com>
2021-03-13 09:11:26 +0200

    Backup.

---
+++ log-replay.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var readline = require("readline");
+var lastTime = undefined;
+
+async function processLineByLine() {
+  const rl = readline.createInterface({
+    input: process.stdin
+  });
+
+  for await (const line of rl) {
+    // Each line in the readline input will be successively available here as
+    // `line`.
+    let d = line.substring(0, line.indexOf(",") - 1);
+    if (lastTime === undefined) {
+      lastTime = new Date(d);
+    } else {
+      let thisTime = new Date(d);
+      let diff = thisTime.getTime() - lastTime.getTime();
+      if (diff > 0) {
+        await new Promise(resolve => setTimeout(resolve, diff));
+      }
+      lastTime = new Date(thisTime);
+    }
+    console.log(line);
+  }
+}
+
+processLineByLine();
